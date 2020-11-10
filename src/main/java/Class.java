dc900b4dbcb17b76fff64603e09920566b8d9dc0
huyenlothi <69282948+huyenlothi@users.noreply.github.com>
2020-11-10 15:34:26 +0700

    add file

---
+++ src/main/java/Class.java
@@ -0,0 +1,9 @@
+public class Class {
+    private String classId;
+    private String className;
+
+    public Class(String classId, String className) {
+        this.classId = classId;
+        this.className = className;
+    }
+}
